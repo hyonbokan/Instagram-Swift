7c1afd7ac92c56b6b359e918a662d66b2451ee55
hyonbokan <mkkanhb@gmail.com>
2023-08-15 18:42:57 +0900

    UI design

---
+++ Instagram/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Michael Kan on 2023/08/15.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
