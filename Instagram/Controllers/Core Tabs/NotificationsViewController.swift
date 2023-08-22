e1f53584718f1b4e90769941e4cd4fce8279b4b5
hyonbokan <mkkanhb@gmail.com>
2023-08-22 14:38:19 +0900

    notification func and UI

--- Instagram/Controllers/Core Tabs/NotificationsViewController.swift
+++ Instagram/Controllers/Core Tabs/NotificationsViewController.swift
@@ -10,17 +10,33 @@ import UIKit
 class NotificationsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     private let tableView: UITableView = {
         let tableView = UITableView()
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.isHidden = false
+        tableView.register(NotificationLikeEventTableViewCell.self, forCellReuseIdentifier: NotificationLikeEventTableViewCell.indentifier)
+        tableView.register(NotificationFollowEventTableViewCell.self, forCellReuseIdentifier: NotificationFollowEventTableViewCell.indentifier)
         return tableView
     }()
     
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.hidesWhenStopped = true
+        spinner.tintColor = .label
+        return spinner
+    }()
+    
+    // lazy - instantiates only when it's called
+    private lazy var noNotificationsView = NoNotificationsView()
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Notifications"
-        // How to hide tabbar title
-        
+        // How 
+        navigationItem.title = "Notifications"
         view.backgroundColor = .systemBackground
+        view.addSubview(spinner)
+//        spinner.startAnimating()
         view.addSubview(tableView)
+//        view.addSubview(noNotificationsView)
         tableView.delegate = self
         tableView.dataSource = self
     }
@@ -28,6 +44,16 @@ class NotificationsViewController: UIViewController, UITableViewDelegate, UITabl
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         tableView.frame = view.bounds
+        spinner.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        spinner.center = view.center
+
+    }
+    
+    private func addNoNotificationsView() {
+        tableView.isHidden = true
+        view.addSubview(tableView)
+        noNotificationsView.frame = CGRect(x: 0, y: 0, width: view.width/2, height: view.width/4)
+        noNotificationsView.center = view.center
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
